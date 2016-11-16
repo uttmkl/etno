d1386bafa16a6755d6c8f3e5681cc88d94bd548b
Den Kezlia <den.kezlia@gmail.com>
2016-11-16 16:12:05 +0200

    Added Facebook Pixel

--- sites/all/themes/etno/templates/html.tpl.php
+++ sites/all/themes/etno/templates/html.tpl.php
@@ -44,6 +44,22 @@
     VK.Widgets.Group("vk_groups", {mode: 0, width: "356", height: "245", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 29725038);
   </script>
   <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+  <!-- Facebook Pixel Code -->
+  <script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+      document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '670839703091401');
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+                 src="https://www.facebook.com/tr?id=670839703091401&ev=PageView&noscript=1"
+    /></noscript>
+  <!-- DO NOT MODIFY -->
+  <!-- End Facebook Pixel Code -->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <div id="fb-root"></div>
